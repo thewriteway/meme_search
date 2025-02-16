12b5fa496ba2267d8645997e48b3f10646304544
Jeremy Watt <jermwatt@gmail.com>
2025-02-16 12:27:26 -0700

    front end model selection ux updates

--- meme_search_pro/meme_search_app/db/migrate/20250216131509_create_image_to_texts.rb
+++ meme_search_pro/meme_search_app/db/migrate/20250216131509_create_image_to_texts.rb
@@ -2,6 +2,7 @@ class CreateImageToTexts < ActiveRecord::Migration[7.2]
   def change
     create_table :image_to_texts do |t|
       t.string :name
+      t.string :resource
       t.string :description
       t.boolean :current , default: false
 
